96df264fad3cc3c1e88015a3c286424831bc600d
Siddharth <siddharthshah1102@gmail.com>
2022-11-21 16:22:37 +0530

    paddle movements

--- main.lua
+++ main.lua
@@ -4,6 +4,8 @@ require("components.Paddle")
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+PADDLE_SPEED = 200
+
 function love.load()
     love.window.setMode(WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
@@ -13,11 +15,32 @@ function love.load()
 
     ball = Ball:init(WINDOW_WIDTH/2 - 5, WINDOW_HEIGHT/2 - 5, 10, 10)
     
-    player1 = Paddle:init(10, 30, 5, 20)
+    player1 = Paddle:init(10, 30, 10, 50)
+    player2 = Paddle:init(WINDOW_WIDTH-10-10, 30, 10, 50)
 end
 
 function love.update(dt)
+
+    -- player1 movement
+    if love.keyboard.isDown('w') then
+        player1.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('s') then
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
+    end
+    
+    -- player2 movement
+    if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
+    end
+
     player1:update(dt)
+    player2:update(dt)
 end
 
 function love.keypressed(key)
@@ -38,4 +61,5 @@ function love.draw()
 
     ball:render()
     player1:render()
+    player2:render()
 end
